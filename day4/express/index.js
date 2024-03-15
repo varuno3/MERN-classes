bba5affdad39717f286a6949f6fe71b7ae6a356d
Varun Badoni <varunbadoni987@gmail.com>
2024-03-15 15:33:28 +0530

    post req

--- day4/express/index.js
+++ day4/express/index.js
@@ -10,7 +10,12 @@ app.get('/', (req, res)=>{
 })   
 app.get('/about', (req, res)=>{
     res.sendFile(__dirname + '/files/html/about.html' );
-})   
+})
+
+app.post('/', (req, res)=>{
+    console.log('a post req was made');
+    res.end('Successfully submitted')
+})
 
 app.listen(port, ()=>{
     console.log(`Listening on port: ${port}`);
